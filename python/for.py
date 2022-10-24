131dc14cafe23f42fae1e2686aaa38f4e6b63122
Willian Danko Caboski <caboskidanko@gmail.com>
2022-10-23 22:02:02 -0300

    Seleção automática

--- python/for.py
+++ python/for.py
@@ -1,13 +1,15 @@
 import random
 import pandas  as pd
-import time 
+import time
 
+
+path = str(r'C:\Users\danko\OneDrive\Documentos\GitHub\OtimizacaoRodizioDeOperacoes\python\VBA.XLSM')
 #                                                                                   7       8           9           10      11          12          
 cabecalho1 = ['Registro','Nome','Area', 'Altura', 'Genero','Carga', 'Incapacidade', 'Over','TiranteE','TiranteD','Macaneta','Capota','Emblemas'] # Aqui estamos informando os nomes das colunas
-Fixos = pd.DataFrame(data = pd.read_excel(r'C:\Users\danko\OneDrive\Documentos\GitHub\OtimizacaoRodizioDeOperacoes\python\VBA.XLSM', 'MEMBROS FIXOS'), columns=cabecalho1) # Aqui estamos criando um DataFrame com as informações do arquivo excel, mudar o endereco da pasta
+Fixos = pd.DataFrame(data = pd.read_excel(path, 'MEMBROS FIXOS'), columns=cabecalho1) # Aqui estamos criando um DataFrame com as informações do arquivo excel, mudar o endereco da pasta
 
 cabecalho2 = ['Registro','Nome','Area', 'Altura', 'Genero','Carga', 'Incapacidade', 'Over','TiranteE','TiranteD','Macaneta','Capota','Emblemas'] # Aqui estamos informando os nomes das colunas
-Estrangeiros = pd.DataFrame(data = pd.read_excel(r'C:\Users\danko\OneDrive\Documentos\GitHub\OtimizacaoRodizioDeOperacoes\python\VBA.XLSM', 'MEMBROS ESTRANGEIROS'), columns=cabecalho2) # Aqui estamos criando um DataFrame com as informações do arquivo excel, mudar o endereco da pasta
+Estrangeiros = pd.DataFrame(data = pd.read_excel(path, 'MEMBROS ESTRANGEIROS'), columns=cabecalho2) # Aqui estamos criando um DataFrame com as informações do arquivo excel, mudar o endereco da pasta
 
 PessOver=[]#lista dos capacitados no overhead
 PessTirE=[]#lista dos capacitados no tirante esquerdo
@@ -35,10 +37,10 @@ selecionados=[]
 
 def selecaoRandom():
     op = 0
-    selecionados=[]
     for p in operacoes:
         if len(operacoes[op]) == 0:
             op+=1
+            print(f'sem pessoas capacitadas para a operação {operacoes[op]}')
             continue
 
         if len(operacoes[op]) == 1:
@@ -97,9 +99,14 @@ while True:
     if c == 0:
         capacitados()
         selecaoRandom()
+        print(selecionados)
         c+=1
     if c >= 1:
         time.sleep(delay)#para a veredura do sistema e increment ao contador
         contTemp += 1 
         if contTemp > TempRod:
+            selecionados.clear()
+            selecaoRandom()
+            capacitados
+            print(selecionados)
             break
