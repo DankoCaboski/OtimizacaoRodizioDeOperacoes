46b490ebb268b4930540ba32f2a56f3f18b36cc6
Willian Danko Caboski <caboskidanko@gmail.com>
2022-11-13 23:39:02 -0300

    integração cartoes

--- python/for.py
+++ python/for.py
@@ -1,11 +1,13 @@
 import random
 import pandas  as pd
 import time
+import  serial 
 
+porta = serial.Serial('COM3', 9600, timeout = 1)
 
 path = str(r'C:\Users\danko\OneDrive\Documentos\GitHub\OtimizacaoRodizioDeOperacoes\python\VBA.XLSM')
 #                                                                                   7       8           9           10      11          12          
-cabecalho1 = ['Registro','Nome','Area', 'Altura', 'Genero','Carga', 'Incapacidade', 'Over','TiranteE','TiranteD','Macaneta','Capota','Emblemas'] # Aqui estamos informando os nomes das colunas
+cabecalho1 = ['Registro','HEX','Nome','Area', 'Altura', 'Genero','Carga', 'Incapacidade', 'Over','TiranteE','TiranteD','Macaneta','Capota','Emblemas'] # Aqui estamos informando os nomes das colunas
 Fixos = pd.DataFrame(data = pd.read_excel(path, 'MEMBROS FIXOS'), columns=cabecalho1) # Aqui estamos criando um DataFrame com as informações do arquivo excel, mudar o endereco da pasta
 
 cabecalho2 = ['Registro','Nome','Area', 'Altura', 'Genero','Carga', 'Incapacidade', 'Over','TiranteE','TiranteD','Macaneta','Capota','Emblemas'] # Aqui estamos informando os nomes das colunas
@@ -21,18 +23,17 @@ PessEmblema=[]#lista dos capacitados nos emblemas
 operacoes=[PessOver, PessTirE, PessTirD, PessMaca, PessCapota, PessEmblema]
 
 def capacitados(): #função q gera lista dos capacitados na funcao desejada
-    c=7
+    c=8
     p = 0       
-    for ii in operacoes:
+    for i in operacoes:
         l = 0 
         for ii in Fixos.itertuples():
             if Fixos.iat[l,c] == 'X':
-                operacoes[p].append(Fixos.iat[l,1])
+                operacoes[p].append(Fixos.iat[l,2])
             l = l+1
         c = c+1
         p = p+1
 
-oper = range(7,12)
 selecionados=[]
 
 def selecaoRandom():
@@ -44,7 +45,8 @@ def selecaoRandom():
             continue
 
         if len(operacoes[op]) == 1:
-            selecionados.append(operacoes[op][0])
+            person = operacoes[op][0]
+            selecionados.append(person)
 #            print(f'operação {op} recebe {operacoes[op][0]}')
             selecionados.append(operacoes[op][0])
             for i in operacoes:
@@ -55,36 +57,29 @@ def selecaoRandom():
 #            print(f'operação {op} recebe {operacoes[op][pess]}')
 
         if len(operacoes[op]) == 2:
-            pess = (random.randint(0,1))
-            selecionados.append(operacoes[op][pess])
+            person = operacoes[op][(random.randint(0,1))]
+            selecionados.append(person)
             for i in operacoes:
                 if i == operacoes[op]:
                     continue
-                elif person in operacoes[op] == True:                        
+                if person in operacoes[op]:                        
                     operacoes[op].remove(person)        
 #            print(f'operação {op} recebe {operacoes[op][pess]}')
 
         if len(operacoes[op])>2:
-            pess = (random.randint(0,len(operacoes[op])-1))
-            person = operacoes[op][pess]
+            person = operacoes[op][random.randint(0,len(operacoes[op])-1)]
             if person in selecionados:
                 while True:
-                    npess = random.randint(0,len(operacoes[op])-1)
-                    person = operacoes[op][npess]
-                    if person in selecionados:
-                        continue
+                    person = operacoes[op][random.randint(0,len(operacoes[op])-1)] 
                     if person not in selecionados:
-                        selecionados.append(person)
-#                        print(f'operação {op} recebe {person}')
                         break
-            if person not in selecionados:
-                selecionados.append(person) 
+            selecionados.append(person) 
 #                print(f'operação {op} recebe {person}')   
-                for i in operacoes:
-                    if i == operacoes[op]:
-                        continue
-                    elif person in operacoes[op] == True:
-                        operacoes[op].remove(person)     
+            for i in operacoes:
+                if i == operacoes[op]:
+                    continue
+                if person in operacoes[op]:
+                    operacoes[op].remove(person)     
         op +=1
 #        print (op)
         if op > 5:
@@ -104,7 +99,7 @@ while True:
         print(selecionados)
         c+=1
         contTemp += 1 
-        Fixos.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
+        #Fixos.to_excel(writer,index=False,header=False,startrow=len(reader)+1)
 
     if c >= 1:
         time.sleep(delay)#para a veredura do sistema e increment ao contador
